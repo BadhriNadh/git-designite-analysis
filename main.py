2a743513bfad735ff5f60e97bf6a0d60c8d20b95
BadhriNadh <42695027+BadhriNadh@users.noreply.github.com>
2023-03-27 12:04:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import subprocess
 import git
 import pandas
@@ -9,19 +5,20 @@ import csv
 
 
 def clone():
-    url = "https://github.com/google/gson"
+    url = "https://github.com/mybatis/mybatis-3.git"
     path = "./repo"
     repo = git.Repo.clone_from(url, path)
     commits = list(repo.iter_commits())
-    header = ['commit', 'ArchitectureSmells', 'DesignSmells', 'ImplementationSmells', 'Smells', 'LOC', 'MAX_LOCM', 'AVG_LCOM', 'MAX_WMC', 'AVG_WMC']
+    header = ['commit', 'ArchitectureSmellsDensity', 'DesignSmellsDensity', 'ImplementationSmellsDensity',
+              'TotalSmells', 'LOC', 'MAX_LOC', 'AVG_LOC', 'MAX_LCOM', 'AVG_LCOM', 'MAX_WMC', 'AVG_WMC']
     with open('report.csv', mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(header)
 
-    for i in range(0, len(commits), round(len(commits) / 11)):
+    for i in range(0, len(commits), round(len(commits) / 10)):
         repo.git.checkout(commits[i])
         start_dj(str(commits[i]) + '-' + str(i))
-        report_generator(str(commits[i]) + '-' + str(i))
+        report_generator(str(commits[i]) + '-' + str(i), url)
 
 
 def start_dj(name):
@@ -29,14 +26,21 @@ def start_dj(name):
     subprocess.run(['java', '-jar', 'DesigniteJava.jar', '-i', './repo', '-o', './' + name], check=True)
 
 
-def report_generator(name):
+def report_generator(name, url):
     architecture_smells = pandas.read_csv('./' + name + '/ArchitectureSmells.csv')
-    design_smells = pandas.read_csv('./' + name + '/DesignSmells.csv', usecols=[0])
+    design_smells = pandas.read_csv('./' + name + '/DesignSmells.csv', usecols=[3])
     implementation_smells = pandas.read_csv('./' + name + '/ImplementationSmells.csv')
     type_metrics = pandas.read_csv('./' + name + '/TypeMetrics.csv')
+    total_loc = type_metrics['LOC'].sum(skipna=False)
     smells = len(architecture_smells) + len(design_smells) + len(implementation_smells)
-    data = [name, len(architecture_smells), len(design_smells), len(implementation_smells), smells,
-            type_metrics['LOC'].sum(skipna=False),
+    data = [name,
+            len(architecture_smells) * 1000 / total_loc,
+            len(design_smells) * 1000 / total_loc,
+            len(implementation_smells) * 1000 / total_loc,
+            smells,
+            total_loc,
+            type_metrics['LOC'].max(skipna=False),
+            type_metrics['LOC'].mean(skipna=False),
             type_metrics.loc[type_metrics['LCOM'] != -1, 'LCOM'].max(skipna=False),
             type_metrics.loc[type_metrics['LCOM'] != -1, 'LCOM'].mean(skipna=False),
             type_metrics.loc[type_metrics['WMC'] != -1, 'WMC'].max(skipna=False),
@@ -44,9 +48,24 @@ def report_generator(name):
     with open('report.csv', mode='a') as file:
         writer = csv.writer(file)
         writer.writerow(data)
+    finder(url, name, design_smells, 'Design Smell', 'Rebellious Hierarchy')
+
+
+def flip():
+    report = pandas.read_csv('report.csv')
+    num_rows = len(report)
+    df_flipped = report.iloc[num_rows - 1::-1]
+    df_flipped.to_csv('report_flipped.csv', index=False)
+
+
+def finder(url, commit, dataframe, smell_type, smell_name):
+    if dataframe[smell_type].str.contains(smell_name).any():
+        data = [url, commit]
+        with open('findings.csv', mode='a') as file:
+            writer = csv.writer(file)
+            writer.writerow(data)
 
 
 if __name__ == '__main__':
     clone()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    flip()
